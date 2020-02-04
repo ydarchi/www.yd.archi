effe26354e61fa0fc682cc81386db2222fe080a6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-02-04 20:39:19 +0100

    Switch from Google Maps to OpenStreetmap.

---
+++ static/plugins/leaflet-map/leaflet.js
@@ -0,0 +1,12 @@
+var mymap = L.map('mapid').setView([50.45966720581055, 3.9412269592285156], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/streets-v11'
+}).addTo(mymap);
+
+L.marker([50.45966720581055, 3.9412269592285156]).addTo(mymap)
+    .bindPopup("<b>YD Architecture</b>").openPopup();
